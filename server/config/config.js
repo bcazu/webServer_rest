cb7c7801ac2a682ff5efe7fe25c7cb5fe57a0dcb
Brayan Castaño <bcastano02@gmail.com>
2019-01-20 21:54:26 -0500

    adicion de variable de entorno mongo a heroku

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB = 'mongodb://localhost:27017/cafe';
 if(process.env.NODE_ENV != 'dev'){
-    urlDB = 'mongodb://cafe-user:**s4bitol4tino**@ds161794.mlab.com:61794/cafe'
+    urlDB = process.env.MONGO_URI;
 }
  //Creo una variable de entorno para almacenar la conexion a la bd
 process.env.URLDB = urlDB;
